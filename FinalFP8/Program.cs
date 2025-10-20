87a66f7b3aabc715ff54a19fbecdbc3e1232bdbd
AGPUERTAS <jeronimo.rojasr.col@upb.edu.co>
2025-10-20 09:48:07 -0500

    Inicio de Final

--- FinalFP8/Program.cs
+++ FinalFP8/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\n===================BIENVENIDO AL TALLER MECANICO EL RORO================");
+        }
+
+        static char menuPrincipal()
+        {
+            Console.WriteLine("\nSeleccione una opcion del menu:");
+            Console.WriteLine("1. Gestion de vehículos");
+            Console.WriteLine("2. Gestion de clientes");
+            Console.WriteLine("3. Gestion de servicios de mantenimiento");
+            Console.WriteLine("4. Salir");
+            
+            char opcion = Char.ToUpper(Console.ReadKey(true).KeyChar);
+            return opcion;
         }
     }
 }
