2f8a8ba14f83a65e0f73d0907e58dc70c079a574
JuanL0914 <Juanl.echeverrir@upb.edu.co>
2025-10-29 09:41:31 -0500

    Clientes

---
+++ FinalFP8/FinalFP8/Program.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualBasic;
+using System.Numerics;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Timers;
+
+namespace FinalFP8
+{
+    internal class Program
+    {
+        static string[,] vehiculo = new string[20, 4];
+        static int contadorVehiculos = 0;
+        
+
+        static void Main(string[] args)
+        {
+            
+
+            menuPrincipal();
+        }
+
+        static void menuPrincipal()
+        {
+            
+            char opcion = ' ';
+
+            Console.WriteLine("\n===================BIENVENIDO AL TALLER MECANICO================");
+
+            Console.WriteLine("Seleccione una opcion del menu:");
+            Console.WriteLine("1. Gestion de vehículos");
+            Console.WriteLine("2. Gestion de clientes");
+            Console.WriteLine("3. Gestion de servicios de mantenimiento");
+            Console.WriteLine("4. Salir");
+
+            opcion = Convert.ToChar(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case '1':
+                    gestionarVehiculos();
+                    break;
+                case '2':
+                    gestionarClientes();
+                    break;
+                case '3':
+                    gestionarServiciosMantenimiento();
+                    break;
+                case '4':
+                    Console.WriteLine("Saliendo del programa. ¡Hasta luego!");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, seleccione una opción del 1 al 4.");
+                    break;
+            }
+
+        }
+
+        static void gestionarVehiculos()
+        {
+            
+            char respuesta = ' ';
+            
+
+            Console.WriteLine("Gestion de vehículos seleccionada.");
+            Console.WriteLine("1. Registrar un nuevo vehículo (marca, modelo, placa, año)");
+            Console.WriteLine("2. Ver lista de vehículos registrados");
+            Console.WriteLine("3. Editar información de un vehículo (buscar por número de placa)");
+            Console.WriteLine("4. Asignar vehículo a un cliente");
+            Console.WriteLine("5. Ver vehículos de un cliente específico");
+            Console.WriteLine("6. Salir de Gestión de vehículos (volver al Menú principal)");
+
+            respuesta = Convert.ToChar(Console.ReadLine());
+
+            switch (respuesta)
+            { 
+              case '1':
+                    
+                    break;
+            }
+        }
+
+        static void registrarVehiculo()
+        {
+            
+            if (contadorVehiculos >= 20)
+            {
+                Console.WriteLine("Ingrese la placa del vehículo:");
+                vehiculo[contadorVehiculos, 0] = Console.ReadLine();
+                Console.WriteLine("Ingrese la marca del vehículo:");
+                vehiculo[contadorVehiculos, 1] = Console.ReadLine();
+                Console.WriteLine("Ingrese el modelo del vehículo:");
+                vehiculo[contadorVehiculos, 2] = Console.ReadLine();
+                Console.WriteLine("Ingrese el año del vehículo:");
+                vehiculo[contadorVehiculos, 3] = Console.ReadLine();
+                contadorVehiculos++;
+            }
+                   
+
+        }
+        
+
+
+
+        static void gestionarClientes()
+        {
+            {
+                int respuesta = 1;
+
+                Console.WriteLine("Gestion de clientes seleccionada.");
+                Console.WriteLine("1. Registrar un nuevo cliente (nombre, cedula, telefono)");
+                Console.WriteLine("2. Ver lista de todos los clientes");
+                Console.WriteLine("3. Editar informacion del cliente)");
+                Console.WriteLine("4. Salir de Gestion de clientes (volver al menu principal)");
+
+                respuesta = Convert.ToInt32(Console.ReadLine());
+
+                if (respuesta == 1)
+                {
+                    for (int i = 0; i < 15; i++)
+                    {
+                        for (int j = 0; j < 15; j++)
+                        {
+                            Console.WriteLine("Ingrese su nombre:");
+                            string nombre = Console.ReadLine();
+                            Console.WriteLine("Ingrese su cedula:");
+                            string cedula = Console.ReadLine();
+                            Console.WriteLine("Ingrese su numero de telefono:");
+                            string telefono = Console.ReadLine();
+                            Console.WriteLine($"Usuario registrado: {nombre} {cedula} {telefono}");
+                        }
+                        gestionarClientes();
+                    }
+                }
+            }
+        }
+
+        static void gestionarServiciosMantenimiento()
+        {
+            Console.WriteLine("Gestion de servicios de mantenimiento seleccionada.");
+        }
+    }
+}
